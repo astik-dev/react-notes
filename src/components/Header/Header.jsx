3eb8f5cb7af27ea066c3863880f3cb532fb4841f
ASTIK <astik.ytt@gmail.com>
2024-03-08 17:27:21 +0100

    Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import classes from "./Header.module.scss";
+
+import Logo from "../Logo/Logo";
+import SearchInput from "../UI/SearchInput/SearchInput";
+
+const Header = () => {
+    
+    return (
+        <header className={classes.header + " container"}>
+            <Logo />
+            <SearchInput placeholder="Search" />
+        </header>
+    )
+};
+
+export default Header;
