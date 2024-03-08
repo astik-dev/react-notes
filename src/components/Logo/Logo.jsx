ab2fda5743e2b477819300287d5e9de9a5142eda
ASTIK <astik.ytt@gmail.com>
2024-03-08 17:18:00 +0100

    Add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import classes from "./Logo.module.scss";
+
+const Logo = () => {
+    
+    return (
+        <div className={classes.logo}>
+            <img src="/react.svg" alt="React icon" />
+            Notes
+        </div>
+    )
+};
+
+export default Logo;
