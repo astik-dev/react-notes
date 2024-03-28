c96fd67a87df31792022a4a3180e66f467ddf3c5
ASTIK <astik.ytt@gmail.com>
2024-03-28 01:01:08 +0100

    Enlarge IconButton size for mobile screens for better visibility

--- src/components/UI/IconButton/IconButton.jsx
+++ src/components/UI/IconButton/IconButton.jsx
@@ -6,8 +6,8 @@ const IconButton = forwardRef(({icon, ...props}, ref) => {
     return (
         <button className={classes.iconButton} ref={ref} {...props}>
             {
-                icon == "delete" ? <img src="/trash.svg" alt="Delete" width="17" height="17" /> :
-                icon == "color" ? <img src="/color.svg" alt="Color" width="19" height="19" /> :
+                icon == "delete" ? <img src="/trash.svg" alt="Delete" className={classes.trash} /> :
+                icon == "color" ? <img src="/color.svg" alt="Color" className={classes.color} /> :
                 ""
             }
         </button>
